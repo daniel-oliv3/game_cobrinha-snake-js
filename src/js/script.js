9a00b32faa34934739b7a9e9426651262dd19386
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-26 09:18:23 -0400

    CTX init

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(0,0,100,100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
