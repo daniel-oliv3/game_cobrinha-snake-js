fe1836137a52fcf118672a53bb9417b9b06c0832
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-26 09:36:37 -0400

    Favicon

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,23 @@
 const canvas = document.querySelector("canvas");
+// contexto de renderização 2d
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(0,0,100,100);
+
+//tamanho do snake
+const size = 30;
+
+//criando a cobrinha
+const snake = [{x: 200, y: 200}];
 
 
+//função responsável por desenhar a cobrinha na tela
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd";
+    ctx.fillRect(snake[0].x, snake[0].y, size, size);
+}
 
 
-
-
-
+drawSnake();
 
 
 
