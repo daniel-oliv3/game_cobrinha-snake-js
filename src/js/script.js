32d1b44998c689b45967f49a62d2876e20e0f922
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-27 09:53:25 -0400

    keyDown movimentação

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,22 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 
-//tamanho da cobrinha
+// tamanho da cobrinha
 const size = 30;
 
-//criando a cobrinha
+// criando a cobrinha
 const snake = [
     {x: 200, y: 200},
     {x: 230, y: 200}
 ];
 
-//direção da cobrinha
-let direction = "right";
+// direção da cobrinha
+let direction;
 
+//looping
+let loopId;
 
-//função responsável por desenhar a cobrinha na tela
+// função responsável por desenhar a cobrinha na tela
 const drawSnake = () => {
     ctx.fillStyle = "#ddd";
     snake.forEach((position, index) => {
@@ -30,29 +32,71 @@ const drawSnake = () => {
 }
 
 
-//função responsável por mover a cobrinha
+// função responsável por mover a cobrinha
 const moveSnake = () => {
+    //verifica se tem direção (!não)
+    if(!direction) return;
+
     const head = snake[snake.length - 1];
 
-    snake.shift();
-
+   // verifica se a direção e igual a direita
     if(direction == "right") {
         snake.push({x: head.x + size, y: head.y});
     }
+    // verifica se a direção e igual a esquerda
+    if(direction == "left") {
+        snake.push({x: head.x - size, y: head.y});
+    }
+    // verifica se a direção e igual a baixo
+    if(direction == "down") {
+        snake.push({x: head.x, y: head.y + size});
+    }
+    // verifica se a direção e igual a cima
+    if(direction == "up") {
+        snake.push({x: head.x, y: head.y - size});
+    }
     
+    snake.shift();
 }
 
 
 
-setInterval(() => {
-    ctx.clearRect(0, 0, 600, 600);
+// função principal loop
+const gameLoop = () => {
+    clearInterval(loopId);
 
+    ctx.clearRect(0, 0, 600, 600);
+    
     moveSnake();
     drawSnake();
-},300);
+
+    loopId = setTimeout(() => {
+        gameLoop();
+    }, 300);
+}
+
+gameLoop();
 
 
-
+// captura as teclas do teclado, quando pressionadas
+document.addEventListener("keydown", ({ key }) => {
+    // verifica se a tecla pressionada é igual a direita
+    if(key == "ArrowRight") {
+        direction = "right";
+    }
+    // verifica se a tecla pressionada é igual a esquerda
+    if(key == "ArrowLeft") {
+        direction = "left";
+    }
+    // verifica se a tecla pressionada é igual a cima
+    if(key == "ArrowUp") {
+        direction = "up";
+    }
+    // verifica se a tecla pressionada é igual a baixo
+    if(key == "ArrowDown") {
+        direction = "down";
+    }
+})
 
 
 
